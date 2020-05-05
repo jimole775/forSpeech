ed9ed891429dc5abe744a5b3e86099985f51b36c
Andy-Super <727346595@163.com>
2020-05-06 02:15:29 +0800

    add demo-web

---
+++ demo-web/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="_img in imgs" :key="_img">
+      <img :src="_img"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import imgs from './imgs'
+import axios from 'axios'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data: () => {
+    console.log(imgs)
+    return {
+      imgs
+    }
+  },
+  created() {
+    axios.get('/test', (res) => {
+      console.log('test:', res)
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
